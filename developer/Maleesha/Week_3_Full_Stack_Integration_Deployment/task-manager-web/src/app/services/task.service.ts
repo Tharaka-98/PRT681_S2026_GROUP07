1c19b222d786b0c66c846bf5ee644427f1d3a5ef
MaleeshaSulakshana <mpmsulakshana98@gmail.com>
2026-09-13 02:42:03 +0930

    Update week 3 api url (render hosted)

--- developer/Maleesha/Week_3_Full_Stack_Integration_Deployment/task-manager-web/src/app/services/task.service.ts
+++ developer/Maleesha/Week_3_Full_Stack_Integration_Deployment/task-manager-web/src/app/services/task.service.ts
@@ -7,7 +7,7 @@ import { TaskItem } from '../models/task.model';
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:8080/api/tasks';
+  private apiUrl = 'http://taskmanager-api-bxgj.onrender.com/api/tasks';
 
   constructor(private http: HttpClient) {}
 
