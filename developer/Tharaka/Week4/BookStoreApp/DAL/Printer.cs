56bc8fb5f221bea7c09ff95eb42a0ffaf6765154
Tharaka-98 <mail3tharaka@gmail.com>
2026-09-14 00:12:12 +0530

    Creating DAL models

---
+++ developer/Tharaka/Week4/BookStoreApp/DAL/Printer.cs
@@ -0,0 +1,9 @@
+namespace BookStoreApp.DAL
+{
+    public class Printer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+    }
+}
